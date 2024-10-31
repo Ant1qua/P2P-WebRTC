8478e79cd1b73c2285a066d88087239da875766a
admin <admin@DESKTOP-N7DDMBU>
2024-10-31 17:51:15 +0300

    Добавьте файлы проекта.

---
+++ P2P WebRTC/Hubs/SignalingHub.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace P2P_WebRTC.Hubs
+{
+    public class SignalingHub : Hub
+    {
+        public async Task SendOffer(string receiverConnectionId, string sdp)
+        {
+            if (string.IsNullOrEmpty(receiverConnectionId))
+            {
+                await Clients.Others
+                    .SendAsync("ReceiveOffer", Context.ConnectionId, sdp);
+            }
+            else
+            {
+                await Clients.Client(receiverConnectionId)
+                  .SendAsync("ReceiveOffer", Context.ConnectionId, sdp);
+            }
+        }
+
+        public async Task SendAnswer(string receiverConnectionId, string sdp)
+        {
+            if (string.IsNullOrEmpty(receiverConnectionId))
+            {
+                await Clients.Others
+                    .SendAsync("ReceiveAnswer", Context.ConnectionId, sdp);
+            }
+            else
+            {
+                await Clients.Client(receiverConnectionId)
+                    .SendAsync("ReceiveAnswer", Context.ConnectionId, sdp);
+            }
+        }
+
+        public async Task SendIceCandidate(string receiverConnectionId, string candidate)
+        {
+            if (string.IsNullOrEmpty(receiverConnectionId))
+            {
+                await Clients.Others
+                    .SendAsync("ReceiveIceCandidate", Context.ConnectionId, candidate);
+            }
+            else
+            {
+                await Clients.Client(receiverConnectionId)
+                  .SendAsync("ReceiveIceCandidate", Context.ConnectionId, candidate);
+            }
+        }
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+    }
+}
---
+++ P2P WebRTC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P2P_WebRTC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
